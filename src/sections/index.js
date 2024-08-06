e408e4278ceca7c677e9cb25fb5284c44ee4f264
Rahul <rahulrajesh@2101.gmail.com>
2024-08-06 13:34:46 +0530

    feat: Add navbar,Services,CustomerSupport page

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+
+
+
+export {
+    Hero,
+    PopularProducts,
+    Subscribe,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    Footer,
+    CustomerReviews
+}
